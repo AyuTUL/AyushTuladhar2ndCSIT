148e82eab98d954e89d4cf42f90e74605530a27b
AyuTUL <ayushtuladhar007@gmail.com>
2024-08-22 20:20:58 +0545

    ih

--- C++/Lab/15.cpp
+++ C++/Lab/15.cpp
@@ -2,24 +2,21 @@
 #define M 5
 #define N 5
 using namespace std;
-int m1,n1,m2,n2,a[M][N],b[M][N],s[M][N]={0},p[M][N]={0}; 
+int m1,n1,m2,n2,a[M][N],b[M][N],s[M][N]={0},p[M][N]={0},i,j,k; 
 void read(int m[][N],int r,int c)
 {
-	int i,j;
 	for(i=0;i<r;i++)
 		for(j=0;j<c;j++)
 			cin>>m[i][j];
 }
-void sum(int a[][N],int b[][N])
+void sum()
 {
-	int i,j;
 	for(i=0;i<m1;i++)
 		for(j=0;j<n1;j++)
 			s[i][j]=a[i][j]+b[i][j];
 }
-void prod(int a[][N],int b[][N])
+void prod()
 {
-	int i,j,k;
 	for(i=0;i<m1;i++)
 		for(j=0;j<n2;j++)
 			for(k=0;k<n1;k++)
@@ -27,7 +24,6 @@ void prod(int a[][N],int b[][N])
 }
 void show(int m[][N],int r,int c)
 {
-	int i,j;
 	for(i=0;i<r;i++)
 	{
 		for(j=0;j<c;j++)
@@ -50,7 +46,7 @@ int main()
 			case 1:
 				cout<<"Enter order of Matrix A : ";
 				cin>>m1>>n1;
-				cout<<"Enter order of Matrix b : ";
+				cout<<"Enter order of Matrix B : ";
 				cin>>m2>>n2;
 				cout<<"Enter elements of Matrix A (="<<m1*n1<<") : ";
 				read(a,m1,n1);
@@ -67,7 +63,7 @@ int main()
 					cout<<"Matrix addition is undefined.";
 					break;
 				}
-				sum(a,b);
+				sum();
 				cout<<"Matrix A : "<<endl;
 				show(a,m1,n1);
 				cout<<"Matrix B : "<<endl;
@@ -81,7 +77,7 @@ int main()
 					cout<<"Matrix multiplication is undefined.";
 					break;
 				}
-				prod(a,b);
+				prod();
 				cout<<"Matrix A : "<<endl;
 				show(a,m1,n1);
 				cout<<"Matrix B : "<<endl;
